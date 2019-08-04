0976dc9a5eededad0a998c2e776e41fd54ab18c4
hi-manshu-kumar <himanshu.kumar394@gmail.com>
2019-08-04 18:19:11 +0530

    --update: added all components

---
+++ src/app/servicePart/mechanic-form/mechanic-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mechanic-form',
+  templateUrl: './mechanic-form.component.html',
+  styleUrls: ['./mechanic-form.component.css']
+})
+export class MechanicFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
