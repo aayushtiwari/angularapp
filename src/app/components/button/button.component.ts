4bfd136c6ddb92ca6509f1e6c96c82d8a924928e
hi-manshu-kumar <himanshu.kumar394@gmail.com>
2019-08-04 22:18:08 +0530

    continue button functional and added guard on it

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-button',
@@ -7,14 +8,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ButtonComponent implements OnInit {
   buttonText: string;
-  link: string;
-  constructor() { }
+  constructor(public router: Router) {
+   }
 
   ngOnInit() {
     this.buttonText = 'Continue';
-    this.link = 'review-order';
+
+    this.router.events.subscribe((event) => {
+      if (event.url === '/review-order' && localStorage.technician && localStorage.technician.length) {
+        this.buttonText = 'Book';
+      }
+      if (event.url === '/review-order' && !localStorage.technician && !localStorage.technician.length) {
+        this.buttonText = 'Continue';
+      }
+      if (event.url === '/') {
+        this.buttonText = 'Continue';
+      }
+      if (event.url === '/select-service') {
+        this.buttonText = 'Continue';
+      }
+      if (event.url === '/service-form') {
+        this.buttonText = 'Review and Book';
+      }
+    });
   }
-  // this.router.url === '/login'
 
-
+  onClick() {
+    if (this.router.url === '/' && localStorage.vehicle && localStorage.vehicle.length) {
+      this.router.navigateByUrl('/select-service');
+    }
+    if (this.router.url === '/select-service' && localStorage.service && localStorage.service.length ) {
+      this.router.navigateByUrl('/service-form');
+    }
+    if (this.router.url === '/service-form' && localStorage.serviceAdd && localStorage.serviceDate &&  localStorage.serviceAdd.length && localStorage.serviceDate.length) {
+      this.router.navigateByUrl('/review-order');
+    }
+    if (this.router.url === '/review-order') {
+      console.log('Confirm Booking');
+    }
+    else{
+      alert('Please Select the Service First');
+    }
+  }
 }
